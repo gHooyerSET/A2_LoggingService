cab095b8096edeeda6f912d53fcda1602f7d198e
Nathan Domingo <Ndomingo6788@conestogac.on.ca>
2022-02-17 13:40:38 -0500

    Added the Logger class and laid out initial logging message functions. Added the Client class and laid out the initial function to send a log to server

---
+++ TestClient/A2_TestClient/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2_TestClient
+{
+    public static class Client
+    {
+        public static string ip { get; private set; } // Ip address of server
+        public static int port { get; private set; } // Port of server
+
+        public static int bufferSize = 1024; // Buffer size for sending log
+
+        static byte[] byteBuffer = new byte[bufferSize]; // Buffer for sending log
+
+        /*
+        * METHOD : SendLog()
+        *
+        * DESCRIPTION : Attempts to connect to server by opening a socket and streaming a log message to server
+        *               Referenced my own client funciton A5Client from Windows Programming 
+        * 
+        * PARAMETERS : string log
+        *
+        * RETURNS :NA
+        */
+        public static void Send(string log)
+        {
+            try
+            {
+                System.Net.Sockets.TcpClient tcpClient = new System.Net.Sockets.TcpClient(ip, port);
+                NetworkStream networkStream = tcpClient.GetStream();
+
+                // Send log message
+                byteBuffer = System.Text.Encoding.ASCII.GetBytes(log);
+                networkStream.Write(byteBuffer, 0, byteBuffer.Length);
+
+                networkStream.Dispose();
+                tcpClient.Close();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nError connectiong with Server\n");
+            }
+        }
+
+    }
+}
